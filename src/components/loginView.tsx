1ca298a2ce5f34c31107b4a33e24233d14fb0a5c
Ashan Kavindu <93070984+Ashan-TUF@users.noreply.github.com>
2025-04-05 23:30:42 +0530

    Update login View

--- src/components/loginView.tsx
+++ src/components/loginView.tsx
@@ -1,7 +1,7 @@
 const LoginView = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center backdrop-blur-xs">
-      <div className="sm:w-6xl bg-[#F7F7F7] rounded-xl shadow-xl p-6">
+      <div className="w-11/12 sm:w-8/12 lg:w-[500px] bg-[#F7F7F7] rounded-xl shadow-xl p-6">
         <h2 className="text-xl font-bold text-center mb-4 text-black">Login</h2>
         {/* Add your login form or buttons here */}
       </div>
