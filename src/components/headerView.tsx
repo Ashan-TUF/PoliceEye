4964232218c6497b79a1be985eb12c83899ff4f1
Ashan Kavindu <93070984+Ashan-TUF@users.noreply.github.com>
2025-04-11 19:20:03 +0530

    update offcanvas

--- src/components/headerView.tsx
+++ src/components/headerView.tsx
@@ -5,6 +5,18 @@ import menuIcon from "../assets/icons/menu.png";
 import BellView from "./bellView";
 import { useState } from "react";
 
+import DashboardIcon from "../assets/icons/dashboard.png";
+import DetectorIcon from "../assets/icons/detector.png";
+import PrisonersIcon from "../assets/icons/prisoners.png";
+import AppointmentsIcon from "../assets/icons/appointments.png";
+import GangsIcon from "../assets/icons/gangs.png";
+import OfficerIcon from "../assets/icons/officer.png";
+import NoticeIcon from "../assets/icons/notice.png";
+import ProfileIcon from "../assets/icons/profile.png";
+import SettingIcon from "../assets/icons/setting.png";
+import MoonIcon from "../assets/icons/moon.png";
+import LogoutIcon from "../assets/icons/logout.png";
+
 const HeaderView = () => {
   const [showOverlay, setShowOverlay] = useState(false);
 
@@ -63,23 +75,118 @@ const HeaderView = () => {
       </nav>
       <div
         className={`fixed top-0 left-0 w-full h-full z-10 bg-transparent transition-opacity duration-800 ${
-          showOverlay ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
+          showOverlay
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
         }`}
         onClick={toggleMenu}
       >
         <div
-          className={`fixed top-0 left-0 bg-white/95 w-64 h-full p-5 shadow-lg transition-transform duration-800 ease-in-out ${
-            showOverlay ? "translate-x-full" : "-translate-x-full"
+          className={`fixed top-0 left-0 bg-white/95 w-64 h-full shadow-lg transition-transform duration-800 ease-in-out ${
+            showOverlay ? "translate-x-0" : "-translate-x-full"
           }`}
           onClick={(e) => e.stopPropagation()}
         >
-          <h2 className="text-lg font-bold mb-4">Menu</h2>
-          <ul className="space-y-2">
-            <li className="cursor-pointer hover:underline">Dashboard</li>
-            <li className="cursor-pointer hover:underline">Search</li>
-            <li className="cursor-pointer hover:underline">Criminals</li>
-            <li className="cursor-pointer hover:underline">Reports</li>
+          <div className="items-center gap-4 p-5 hover:opacity-50 cursor-pointer mb-5">
+            <img
+              src={sriLankaLogo}
+              alt="Sri Lanka Logo"
+              className="w-12 h-12 blur-xs"
+            />
+            <span className="text-xl font-semibold text-[#2C2C2C]">
+              CEYLON POLICE
+            </span>
+          </div>
+          <ul className="space-y-2 p-5">
+            <li className="cursor-pointer hover:opacity-50">
+              <div className="flex gap-5 items-center">
+                <img src={DashboardIcon} alt="dashboard" className="w-8 h-8" />
+                <span className="text-base">Dashboard</span>
+              </div>
+            </li>
+            <li className="cursor-pointer hover:opacity-50">
+              <div className="flex gap-5 items-center">
+                <img
+                  src={DetectorIcon}
+                  alt="Criminal Detector Icon"
+                  className="w-8 h-8"
+                />
+                <span className="text-base">Criminal Detector</span>
+              </div>
+            </li>
+            <li className="cursor-pointer hover:opacity-50">
+              <div className="flex gap-5 items-center">
+                <img
+                  src={PrisonersIcon}
+                  alt="Prisoners Icon"
+                  className="w-8 h-8"
+                />
+                <span className="text-base">Manage Prisoners</span>
+              </div>
+            </li>
+            <li className="cursor-pointer hover:opacity-50">
+              <div className="flex gap-5 items-center">
+                <img
+                  src={AppointmentsIcon}
+                  alt="Manage Appointment Icon"
+                  className="w-8 h-8"
+                />
+                <span className="text-base">Manage Appointments</span>
+              </div>
+            </li>
+            <li className="cursor-pointer hover:opacity-50">
+              <div className="flex gap-5 items-center">
+                <img
+                  src={GangsIcon}
+                  alt="Identified Gangs Icon"
+                  className="w-8 h-8"
+                />
+                <span className="text-base">Identified Gangs</span>
+              </div>
+            </li>
+            <li className="cursor-pointer hover:opacity-50">
+              <div className="flex gap-5 items-center">
+                <img
+                  src={OfficerIcon}
+                  alt="Manage Officers Icon"
+                  className="w-8 h-8"
+                />
+                <span className="text-base">Manage Officers</span>
+              </div>
+            </li>
+            <li className="cursor-pointer hover:opacity-50">
+              <div className="flex gap-5 items-center">
+                <img
+                  src={NoticeIcon}
+                  alt="Manage Notice Board Icon"
+                  className="w-8 h-8"
+                />
+                <span className="text-base">Manage Notice Board</span>
+              </div>
+            </li>
           </ul>
+          <div className="fixed flex w-full justify-between p-5 bottom-10">
+            <img
+              src={ProfileIcon}
+              alt="Profile Icon"
+              className="w-8 h-8 hover:opacity-50 cursor-pointer"
+            />
+            <img
+              src={SettingIcon}
+              alt="Setting Icon"
+              className="w-8 h-8 hover:opacity-50 cursor-pointer"
+            />
+            <img
+              src={MoonIcon}
+              alt="Moon Icon"
+              className="w-8 h-8 hover:opacity-50 cursor-pointer"
+            />
+            <img
+              src={LogoutIcon}
+              alt="Logout Icon"
+              className="w-8 h-8 hover:opacity-50 cursor-pointer"
+            />
+          </div>
         </div>
       </div>
     </>
--- src/ui/homeScreen.tsx
+++ src/ui/homeScreen.tsx
@@ -1,23 +1,23 @@
-// import sriLankaLogo from "../../src/assets/images/sriLnkaPoliceLogo.png";
+import sriLankaLogo from "../../src/assets/images/sriLnkaPoliceLogo.png";
 import HeaderView from "../components/headerView";
-// import MenuBarView from "../components/menuBarView";
-// import dashboardIcon from "../assets/icons/dashboard.png";
-// import detectorIcon from "../assets/icons/detector.png";
-// import prisonersIcon from "../assets/icons/prisoners.png";
-// import appointmentsIcon from "../assets/icons/appointments.png";
-// import gangsIcon from "../assets/icons/gangs.png";
-// import officerIcon from "../assets/icons/officer.png";
-// import noticeIcon from "../assets/icons/notice.png";
-// import menuIcon from "../assets/icons/menu.png";
-// import profileIcon from "../assets/icons/profile.png";
-// import settingIcon from "../assets/icons/setting.png";
-// import moonIcon from "../assets/icons/moon.png";
-// import logoutIcon from "../assets/icons/logout.png";
+import MenuBarView from "../components/menuBarView";
+import dashboardIcon from "../assets/icons/dashboard.png";
+import detectorIcon from "../assets/icons/detector.png";
+import prisonersIcon from "../assets/icons/prisoners.png";
+import appointmentsIcon from "../assets/icons/appointments.png";
+import gangsIcon from "../assets/icons/gangs.png";
+import officerIcon from "../assets/icons/officer.png";
+import noticeIcon from "../assets/icons/notice.png";
+import menuIcon from "../assets/icons/menu.png";
+import profileIcon from "../assets/icons/profile.png";
+import settingIcon from "../assets/icons/setting.png";
+import moonIcon from "../assets/icons/moon.png";
+import logoutIcon from "../assets/icons/logout.png";
 const HomeScreen = () => {
   return (
     <div className="w-screen min-h-screen overflow-x-hidden">
       <HeaderView />
-      {/* <div className="flex w-full pt-5">
+      <div className="flex w-full pt-5">
         <div className="bg-white h-fit w-10 ml-2 pt-5 pb-5 top-5 rounded-4xl flex flex-col gap-4 items-center justify-center">
           <img
             src={menuIcon}
@@ -110,7 +110,7 @@ const HomeScreen = () => {
           />
           <p className="text-xl text-[#2C2C2C] mt-4">CEYLON POLICE</p>
         </div>
-      </div> */}
+      </div>
       <p className="fixed w-full bottom-5 text-sm text-center text-[#2C2C2C]">
         Developed by <b>Ashan Kavindu</b> | © 2025.
       </p>
