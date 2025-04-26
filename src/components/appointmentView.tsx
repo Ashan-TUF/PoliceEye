6ef61179253a8d4e4c58b1aaec1f27290e88cee9
Ashan Kavindu <93070984+Ashan-TUF@users.noreply.github.com>
2025-04-26 21:45:08 +0530

    add appointments page

---
+++ src/components/appointmentView.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import eyeIcon from "../assets/icons/eye.png";
+import alertIcon from "../assets/icons/alert.png";
+
+export interface AppointmentViewProps {
+  appointmentId: string;
+  visitorFullName: string;
+  visitorNic: string;
+  prisonerCode: string;
+  prisonerName: string;
+  appointmentDate: Date;
+  appointmentTime: string;
+  purposeOfVisit: "Family" | "Lawyer" | "Official" | "Medical";
+  approvedByOfficer: string;
+  status: "Pending" | "Approved" | "Rejected" | "Completed" | "Cancelled";
+  createdAt: string;
+  updateAt: string;
+}
+
+interface Props {
+  data: AppointmentViewProps;
+  onViewAppointment?: (appointmentId: string) => void;
+}
+
+export const AppointmentView: React.FC<Props> = ({
+  data,
+  onViewAppointment,
+}) => {
+  return (
+    <div className="flex flex-col rounded-2xl bg-white p-5 shadow-md relative">
+      <div className="flex flex-col gap-1">
+        {Object.entries(data).map(([key, value]) => (
+          <p className="text-sm text-gray-700" key={key}>
+            <span className="font-semibold">
+              {key
+                .replace(/([A-Z])/g, " $1")
+                .replace(/^./, (str) => str.toUpperCase())}
+              :
+            </span>{" "}
+            {value instanceof Date ? value.toDateString() : value}
+          </p>
+        ))}
+      </div>
+
+      <div className="absolute top-5 right-5">
+        <button
+          onClick={() => onViewAppointment?.(data.appointmentId)}
+          aria-label={`View appointment ${data.appointmentId}`}
+          className="p-2 rounded hover:bg-gray-100 transition cursor-pointer"
+        >
+          <img src={eyeIcon} alt="view icon" className="w-5 h-5" />
+        </button>
+      </div>
+      <div className="absolute top-15 right-5 p-2">
+        <img src={alertIcon} alt="alert icon" className="w-5 h-5" />
+      </div>
+      <div className="absolute bottom-5 right-5 p-2">
+        <span className="text-xs text-[#707070]">
+          Appointment number: {data.appointmentId}
+        </span>
+      </div>
+    </div>
+  );
+};
--- src/scenes/appointments/index.tsx
+++ src/scenes/appointments/index.tsx
@@ -1,19 +1,155 @@
 import Footerbase from "../../components/footerbase";
 import HeaderView from "../../components/headerView";
-import MainManu from "../../components/mainMenu";
+import MainMenu from "../../components/mainMenu";
+import alertIcon from "../../assets/icons/alert.png";
+import { AppointmentView } from "../../components/appointmentView";
 
 export const AppointmentsScreen = () => {
-    return (
-        <div className="w-screen h-screen overflow-x-hidden">
+  const appointmentData = [
+    {
+      appointmentId: "12345",
+      visitorFullName: "John Doe",
+      visitorNic: "200123456V",
+      prisonerCode: "P001",
+      prisonerName: "Ashan Kavindu Kalupahana",
+      appointmentDate: new Date(),
+      appointmentTime: "10:00 AM",
+      purposeOfVisit: "Family",
+      approvedByOfficer: "Officer Smith",
+      status: "Approved",
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+    },
+    {
+      appointmentId: "36987",
+      visitorFullName: "John Doe",
+      visitorNic: "200123456V",
+      prisonerCode: "P001",
+      prisonerName: "Ashan Kavindu Kalupahana",
+      appointmentDate: new Date(),
+      appointmentTime: "10:00 AM",
+      purposeOfVisit: "Family",
+      approvedByOfficer: "Officer Smith",
+      status: "Approved",
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+    },
+    {
+      appointmentId: "36987",
+      visitorFullName: "John Doe",
+      visitorNic: "200123456V",
+      prisonerCode: "P001",
+      prisonerName: "Ashan Kavindu Kalupahana",
+      appointmentDate: new Date(),
+      appointmentTime: "10:00 AM",
+      purposeOfVisit: "Family",
+      approvedByOfficer: "Officer Smith",
+      status: "Approved",
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+    },
+    {
+      appointmentId: "36987",
+      visitorFullName: "John Doe",
+      visitorNic: "200123456V",
+      prisonerCode: "P001",
+      prisonerName: "Ashan Kavindu Kalupahana",
+      appointmentDate: new Date(),
+      appointmentTime: "10:00 AM",
+      purposeOfVisit: "Family",
+      approvedByOfficer: "Officer Smith",
+      status: "Approved",
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+    },
+    {
+      appointmentId: "36987",
+      visitorFullName: "John Doe",
+      visitorNic: "200123456V",
+      prisonerCode: "P001",
+      prisonerName: "Ashan Kavindu Kalupahana",
+      appointmentDate: new Date(),
+      appointmentTime: "10:00 AM",
+      purposeOfVisit: "Family",
+      approvedByOfficer: "Officer Smith",
+      status: "Approved",
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+    },
+    {
+      appointmentId: "36987",
+      visitorFullName: "John Doe",
+      visitorNic: "200123456V",
+      prisonerCode: "P001",
+      prisonerName: "Ashan Kavindu Kalupahana",
+      appointmentDate: new Date(),
+      appointmentTime: "10:00 AM",
+      purposeOfVisit: "Family",
+      approvedByOfficer: "Officer Smith",
+      status: "Approved",
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+    },
+    {
+      appointmentId: "36987",
+      visitorFullName: "John Doe",
+      visitorNic: "200123456V",
+      prisonerCode: "P001",
+      prisonerName: "Ashan Kavindu Kalupahana",
+      appointmentDate: new Date(),
+      appointmentTime: "10:00 AM",
+      purposeOfVisit: "Family",
+      approvedByOfficer: "Officer Smith",
+      status: "Approved",
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+    },
+    {
+      appointmentId: "36987",
+      visitorFullName: "John Doe",
+      visitorNic: "200123456V",
+      prisonerCode: "P001",
+      prisonerName: "Ashan Kavindu Kalupahana",
+      appointmentDate: new Date(),
+      appointmentTime: "10:00 AM",
+      purposeOfVisit: "Family",
+      approvedByOfficer: "Officer Smith",
+      status: "Approved",
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+    },
+  ] as const;
+
+  return (
+    <div className="w-screen h-screen overflow-x-hidden flex flex-col">
       <HeaderView />
-      <div className="w-full pt-5 flex overflow-y-auto">
-        <MainManu />
+      <div className="flex flex-1 overflow-y-auto pt-5 mb-16">
+        <MainMenu />
         <div className="flex flex-col w-full">
-          
-          {/* <PrisonerProfileView /> */}
+          <div className="flex justify-between items-center px-10 py-4">
+            <p className="text-2xl font-semibold text-gray-800">Appointments</p>
+            <button
+              className="flex items-center gap-2 p-2 bg-gray-100 rounded-md hover:bg-gray-200 transition duration-500 cursor-pointer shadow-md"
+              aria-label="View warnings"
+            >
+              <span className="text-sm font-medium text-gray-800">
+                Warnings
+              </span>
+              <img src={alertIcon} alt="Warning icon" className="w-5 h-5" />
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-2 w-full p-2 xl:ps-10 h-fit">
+            {appointmentData.map((appointment) => (
+              <AppointmentView
+                key={appointment.visitorNic}
+                data={appointment}
+              />
+            ))}
+          </div>
         </div>
       </div>
       <Footerbase />
     </div>
-    );
-}
+  );
+};
